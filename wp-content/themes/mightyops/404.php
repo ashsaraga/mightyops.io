5b4b192b21ed51227e59ab9071595d8876103043
ashsaraga <ashsaraga@gmail.com>
2019-04-25 09:58:10 -0400

    404

--- wp-content/themes/mightyops/404.php
+++ wp-content/themes/mightyops/404.php
@@ -21,7 +21,7 @@ get_header();
 				<p class="has-margin-6 is-size-4">This page doesn't exist yet, but we can get you back to the known universe.</p>
 				<div class="is-flex has-padding-1">
 					<a class="fits-flex-33 has-background-evening has-text-daybreak is-uppercase has-text-centered is-size-6 has-letter-spacing-1 has-padding-8 has-margin-top has-margin-8 has-underline-mod2 disapparateLink">Go Back</a>
-					<a href="<?php echo home_url(); ?>" class="fits-flex-33 has-background-evening has-text-daybreak is-uppercase has-text-centered is-size-6 has-letter-spacing-1 has-padding-8 has-margin-top has-margin-8 has-underline-mod4">Head Home</a>
+					<a href="<?php echo home_url(); ?>" class="fits-flex-33 has-background-evening has-text-daybreak is-uppercase has-text-centered is-size-6 has-letter-spacing-1 has-padding-8 has-margin-top has-margin-8 has-underline-mod4">Warp Home</a>
 				</div>
 			</div>
 		</div>
